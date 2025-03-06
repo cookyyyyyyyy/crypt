175ac91a11b3f5c1fca57eeb8cb198903ed9db21
cookyyyyyyyy <speedygond1@gmail.com>
2025-03-06 23:14:55 +0100

    add ecc generate key

---
+++ ECC/generate-key.py
@@ -0,0 +1,21 @@
+from Crypto.PublicKey import ECC
+
+def export_privkey(privkey, filename):
+    with open(filename, "wt") as file:
+        pwd = b'secret'
+        key_data = privkey.export_key(format='PEM',
+                                passphrase=pwd,
+                                protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
+                                prot_params={'iteration_count':131072})
+        file.write(key_data)
+
+def export_pubkey(pubkey, filename):
+    with open(filename, "wt") as file:
+        key_data = pubkey.export_key(format='PEM')
+        file.write(key_data)
+
+Keypair = ECC.generate(curve='p256')
+pub_Keypair = Keypair.public_key()
+
+export_privkey(Keypair, 'priv_key.pem')
+export_pubkey(pub_Keypair, 'pub_key.pem')
