de03b8fdcdc7e8ea85abd94cb5a696e73a13aebc
cookyyyyyyyy <speedygond1@gmail.com>
2025-03-06 22:56:48 +0100

    Create generate-key.py

---
+++ generate-key.py
@@ -0,0 +1,20 @@
+from Crypto.PublicKey import RSA
+
+def export_privkey(privkey, filename):
+    with open(filename, "wb") as file:
+        file.write(privkey.export_key(format='PEM',
+                                      passphrase='MyPassPhrase',
+                                      protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
+                                      prot_params={'iteration_count':131072}))
+        file.close()
+
+def export_pubkey(pubkey, filename):
+    with open(filename, "wb") as file:
+        file.write(pubkey.export_key())
+        file.close()
+
+Keypair = RSA.generate(3072)
+pub_Keypair = Keypair.publickey()
+
+export_privkey(Keypair, 'priv_key.pem')
+export_pubkey(pub_Keypair, 'pub_key.pem')
